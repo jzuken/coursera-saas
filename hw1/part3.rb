a615a2cf9c0266ec93ee493576146fce07cec422
jzuken <timofey.bryksin@gmail.com>
2012-06-03 20:35:33 +0400

    made anagrams case-insensitive

--- hw1/part3.rb
+++ hw1/part3.rb
@@ -1,15 +1,12 @@
 def combine_anagrams(words)
   hash = Hash.new [] 
   words.each do |w|
-    if hash.include?(w.chars.sort) 
-      hash[w.chars.sort].push(w)
+    characters = w.downcase.chars.sort
+    if hash.include?(characters) 
+      hash[characters].push(w)
     else
-      hash[w.chars.sort] = [w]
+      hash[characters] = [w]
     end    
   end
   hash.values
 end
-
-
-print combine_anagrams ['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream']
-# => output: [["cars", "racs", "scar"], ["four"], ["for"], ["potatoes"],
